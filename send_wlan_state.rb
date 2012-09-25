93af6a0ff4a69d85c41169da30299deb43f5d790
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-25 16:22:43 +0300

    Run in a loop

--- send_wlan_state.rb
+++ send_wlan_state.rb
@@ -3,27 +3,68 @@
 require "pp"
 require "socket"
 
-sock = UDPSocket.new
-sock.connect "eventlog", 3858
+HOST = "eventlog"
+PORT = 3858
 
-output =  `hostapd_cli all_sta`
-if not $?.success?
-  STDERR.puts "failed to execute 'hostapd_cli all_sta'"
-  exit 1
+
+def device_list
+  output =  `hostapd_cli all_sta`
+  if not $?.success?
+    STDERR.puts "failed to execute 'hostapd_cli all_sta'"
+    exit 1
+  end
+
+  devices = output.split("\n").select do |line|
+    line.start_with? "dot11RSNAStatsSTAAddress"
+  end.map do |line|
+    line.split("=")[1]
+  end
 end
 
-devices = output.split("\n").select do |line|
-  line.start_with? "dot11RSNAStatsSTAAddress"
-end.map do |line|
-  line.split("=")[1]
-end.join(",")
+class DevicePoller
 
-packet = <<EOF
+  def initialize(host, port)
+    @sock = UDPSocket.new
+    @sock.connect host, port
+  end
+
+  def poll
+    @devices = device_list
+
+    hashkey = @devices.reduce do |memo, mac|
+      memo+mac
+    end
+
+    if @prev_hashkey != hashkey
+      @prev_hashkey = hashkey
+      send
+    end
+
+  end
+
+  def send
+
+    packet = <<EOF
 type:wlan
 wlan_event:hotspot_state
-connected_devices:[#{ devices }]
+connected_devices:[#{ @devices.join(",") }]
 hostname:#{ Socket.gethostname }
 EOF
+    puts packet
+    @sock.send packet, 0
+  end
 
-puts packet
-sock.send packet, 0
+  def loop
+    while true
+      poll
+      sleep 60
+    end
+  end
+
+end
+
+
+if __FILE__ == $0
+  poller = DevicePoller.new HOST, PORT
+  poller.loop()
+end
