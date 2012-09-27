a27db0a5aee752e68150ce87a7714a29787b6cea
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-27 08:42:35 +0300

    Update builddeb.sh

--- builddeb.sh
+++ builddeb.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# Run from parent directory!
+
+FILES="wlan-poller/Makefile wlan-poller/wlan-poller.rb wlan-poller/debian/"
+
 set -eu
 
+# Remove previous build
 rm -rf wlan-poller_build/ || true
 
 mkdir wlan-poller_build
-cp -r wlan-poller/Makefile wlan-poller/wlan-poller.rb wlan-poller/debian/ wlan-poller_build/
+
+cp -r $FILES wlan-poller_build/
+
 cd wlan-poller_build/
+
 debuild -us -uc
 
