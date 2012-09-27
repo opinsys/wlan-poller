e4f91667b024ca66752d6b9e9434d75ec612aa30
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-27 08:18:31 +0300

    Clean build dir before new build

--- builddeb.sh
+++ builddeb.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -eu
+
+rm -rf wlan-poller_build/ || true
+
 mkdir wlan-poller_build
 cp -r wlan-poller/Makefile wlan-poller/wlan-poller.rb wlan-poller/debian/ wlan-poller_build/
 cd wlan-poller_build/
